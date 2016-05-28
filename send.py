2348661f965da9979f82a8053e0a4d2b4b85a799
Kirill Ignatev <kirill.ignatev@datarobot.com>
2016-05-28 19:33:41 +0300

    PEP8

--- send.py
+++ send.py
@@ -1,28 +1,30 @@
 import tornado.httpserver
 import tornado.websocket
 import tornado.ioloop
-from tornado.ioloop import PeriodicCallback
 import tornado.web
-from random import randint #Random generator
+from tornado.ioloop import PeriodicCallback
+from random import randint  # Random generator
 
-#Config
-port = 9000 #Websocket Port
-timeInterval= 2000 #Milliseconds
+# Config
+port = 9000  # Websocket Port
+timeInterval = 2000  # Milliseconds
+
 
 class WSHandler(tornado.websocket.WebSocketHandler):
-	#check_origin fixes an error 403 with Tornado
-	#http://stackoverflow.com/questions/24851207/tornado-403-get-warning-when-opening-websocket
+    # check_origin fixes an error 403 with Tornado
+    # http://stackoverflow.com/questions/24851207/tornado-403-get-warning-when-opening-websocket
     def check_origin(self, origin):
         return True
 
     def open(self):
-		#Send message periodic via socket upon a time interval
+        # Send message periodic via socket upon a time interval
         self.callback = PeriodicCallback(self.send_values, timeInterval)
         self.callback.start()
 
     def send_values(self):
-		#Generates random values to send via websocket
-        self.write_message(str(randint(1,10)) + ';' + str(randint(1,10)) + ';' + str(randint(1,10)) + ';' + str(randint(1,10)))
+        # Generates random values to send via websocket
+        self.write_message(str(randint(1, 10)) + ';' + str(randint(1, 10)) + ';' +
+                           str(randint(1, 10)) + ';' + str(randint(1, 10)))
 
     def on_message(self, message):
         pass
